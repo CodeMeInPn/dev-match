0896e813ffef3485bf9302c59a4679088ce85be9
CodeMeInPn <codemeinpn@gmail.com>
2026-06-27 21:10:08 +0200

    feat: add post create profile endpoint

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
 
 @Controller('profiles')
 export class ProfilesController {
@@ -9,5 +10,12 @@ export class ProfilesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return { id };
+  }
+  @Post()
+  create(@Body() createProfileDto: CreateProfileDto) {
+    return {
+      name: createProfileDto.name,
+      description: createProfileDto.description,
+    };
   }
 }
